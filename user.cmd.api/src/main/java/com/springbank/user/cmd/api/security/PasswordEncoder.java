16953b097c5e9727827f42cc2010125fb55b1cb8
Mirek Krenc <kremir@o2.pl>
2021-02-01 18:04:41 +0100

    added events and aggregate

---
+++ user.cmd.api/src/main/java/com/springbank/user/cmd/api/security/PasswordEncoder.java
@@ -0,0 +1,6 @@
+package com.springbank.user.cmd.api.security;
+
+public interface PasswordEncoder {
+
+    String hashedPassword(String password);
+}
---
+++ user.cmd.api/src/main/java/com/springbank/user/cmd/api/security/PasswordEncoderImpl.java
@@ -0,0 +1,15 @@
+package com.springbank.user.cmd.api.security;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PasswordEncoderImpl implements PasswordEncoder {
+    @Override
+    public String hashedPassword(String password) {
+        var bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
+        var hashedPassword = bCryptPasswordEncoder.encode(password);
+
+        return hashedPassword;
+    }
+}
